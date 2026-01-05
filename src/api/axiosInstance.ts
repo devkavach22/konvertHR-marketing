4918c692858b11841c5c8a9688a617ef53833962
devkavach22 <developer@kavachglobal.com>
2026-01-05 11:53:53 +0530

    Auth API Changes

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -20,7 +20,7 @@ axiosInstance.interceptors.request.use(
         const response = await axios.post(
           // "http://192.168.11.150:4000/api/auth",
           // "http://178.236.185.232:4000/api/auth",
-          "https://konverthrnode.onrender.com",
+          "https://konverthrnode.onrender.com/api/auth",
 
           { user_name: "dhaval" },
           { headers: { "Content-Type": "application/json" } } // prevent interceptor reuse
